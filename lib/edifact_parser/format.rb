771723c7498aac11332a1c55a6b22fa07f127688
Jan Riethmayer <jan@riethmayer.de>
2017-06-07 23:52:02 +0100

    Update format.rb
    remove DAM as it ocurres in text

--- lib/edifact_parser/format.rb
+++ lib/edifact_parser/format.rb
@@ -4,7 +4,7 @@ module EdifactParser
       'UNB', 'UNH', 'UNZ', 'UNT', 'UNS', 'IFI', 'IFT', 'ODI', 'TVL', 'APD',
       'ADR', 'AGR', 'AJT', 'ALC', 'ALI', 'APP', 'ARD', 'ARR', 'ASI', 'ATT', 'AUT',
       'BAS', 'BGM', 'BII', 'BUS', 'CAV', 'CCD', 'CCI', 'CDI', 'CDS', 'CDV', 'CED', 'CIN',
-      'CLA', 'CLI', 'CMP', 'CNI', 'CNT', 'COD', 'COM', 'COT', 'CPI', 'CPS', 'CPT', 'CST',
+      'CLA', 'CLI', 'CMP', 'CNI', 'CNT', 'COD', 'COM', 'COT', 'CPI', 'CPT', 'CST',
       'CTA', 'CUX', 'DFN', 'DGS', 'DII', 'DIM', 'DLI', 'DLM', 'DMS', 'DOC', 'DRD',
       'DSG', 'DSI', 'DTM', 'EDT', 'EFI', 'ELM', 'ELU', 'ELV', 'EMP', 'EQA', 'EQD', 'EQN',
       'ERC', 'ERP', 'EVE', 'FCA', 'FII', 'FNS', 'FNT', 'FOR', 'FSQ', 'FTX', 'GDS', 'GEI',
